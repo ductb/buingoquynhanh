35fe384d689a4bb788272622cf2ef4baf70bdd83
ductb <88604254+ductb@users.noreply.github.com>
2021-08-08 14:57:38 +0700

    Update config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ const CONFIG = {
     btnReply: 'Gửi cho anh <3',
     reply: 'Trên thế giới hơn 7 tỉ người nhưng đối với em thì anh là quan trọng nhất♥️',
     mess: 'Anh biết thế nào em cũng trả lời vậy mà',
-    messDesc: 'Thế sau này lấy anh nhá. ',
+    messDesc: 'Thế sau này lấy anh nhá! ',
     btnAccept: 'Đồng ý',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
